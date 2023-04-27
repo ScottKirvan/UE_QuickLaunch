312f5bc7bfee3994493624e02341263642649382
Scott Kirvan <scott@skvfx.com>
2023-04-26 18:47:03 -0700

    look ma, an uninstaller

--- uninstall.bat
+++ uninstall.bat
@@ -1,2 +1,27 @@
 @echo off
-REGEDIT.exe /s "%~dp0\src\remove-folder-keys.reg"
+set regfile="%~dp0\src\remove-folder-keys.reg"
+
+echo Deleting the following registry keys and all associated subkeys:
+echo (These are from %regfile%)
+echo ===========================================================================
+type %regfile%
+echo.
+echo ===========================================================================
+
+:again
+set /p answer=Continue and apply these settings using regedit.exe? (Y/N)?
+if /i "%answer:~,1%" EQU "Y" goto runit
+if /i "%answer:~,1%" EQU "N" goto finished
+echo Please type Y for Yes, or N for No
+goto again
+
+:runit
+echo.
+echo Calling %regfile%
+REGEDIT.exe /s %regfile%
+
+:finished
+echo.
+echo Finished
+echo.
+pause
